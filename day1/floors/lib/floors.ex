b6e36c49079197b44a377d39cfd3f5e61711c943
Andrew Kraut <akraut@gmail.com>
2016-03-21 23:33:25 -0700

    Update implementation for basement tracking

--- day1/floors/lib/floors.ex
+++ day1/floors/lib/floors.ex
@@ -1,35 +1,35 @@
 defmodule Floors do
   def start_link do
-    Agent.start_link(fn -> 0 end, name: __MODULE__)
+    Agent.start_link(fn -> {0,0,0} end, name: __MODULE__)
   end
 
   def go_up do
-    Agent.update(__MODULE__, fn floor -> floor+1 end)
+    Agent.update(__MODULE__,
+      fn {floor, basement, operations} -> {floor+1, basement, operations+1} end)
   end
 
   def go_down do
-    Agent.update(__MODULE__, fn floor -> floor-1 end)
+    Agent.update(__MODULE__, fn state -> do_down(state) end)
   end
 
-    Agent.get(__MODULE__, fn floor -> floor end)
   def floor? do
+    Agent.get(__MODULE__, fn {floor, _basement, _operations} -> floor end)
+  end
+
+  def basement? do
+    Agent.get(__MODULE__, fn {_floor, basement, _operations} -> basement end)
+  end
+
+  def operations? do
+    Agent.get(__MODULE__, fn {_floor, _basement, operations} -> operations end)
   end
 
   def reset do
-    Agent.update(__MODULE__, fn _ -> 0 end)
+    Agent.update(__MODULE__, fn _ -> {0,0,0} end)
   end
 
   def process(directions) when is_list(directions) do
     :ok = Floors.reset
-    do_process(directions)
-    Floors.what_floor?
-  end
-
-  def process(directions) do
-    process(to_char_list(directions))
-  end
-
-  defp do_process(directions) do
     for dir <- directions do
       case dir do
         ?( -> Floors.go_up
@@ -37,5 +37,21 @@ defmodule Floors do
         _ -> dir
       end
     end
+    :ok
+  end
+
+  def process(directions) do
+    process(to_char_list(directions))
+  end
+
+  defp do_down({floor, 0, operations}) do
+    case floor do
+      0 -> {floor-1, operations+1, operations+1}
+      _ -> {floor-1, 0, operations+1}
+    end
+  end
+
+  defp do_down({floor, basement, operations}) do
+    {floor-1, basement, operations+1}
   end
 end
