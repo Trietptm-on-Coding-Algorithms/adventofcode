7c92fe9bc994f7b5bbac5e09e3f984a0c4d58ea0
Andrew Kraut <akraut@pandora.com>
2016-03-22 15:56:47 -0700

    Alter Mix task to run both totals in parallel

--- day2/paper/lib/mix/tasks/paper.ex
+++ day2/paper/lib/mix/tasks/paper.ex
@@ -6,14 +6,21 @@ defmodule Mix.Tasks.Paper.Manifest do
   def run(args) do
     File.stream!(args)
     |> Enum.map(&clean_entry(&1))
-    |> Enum.map(&Paper.total?(&1))
-    |> Enum.sum
-    |> IO.puts
+    |> Enum.map(&get_totals(&1))
+    |> Enum.reduce(fn({paper, ribbon}, {p_acc, r_acc}) -> {paper + p_acc, ribbon + r_acc} end)
+    |> IO.inspect
   end
 
   defp clean_entry (entry) do
     entry
     |> to_string
     |> String.strip
+  end
+
+  defp get_totals(dimensions) do
+    {
+      Paper.total?(dimensions),
+      Paper.total_ribbon?(dimensions)
+    }
   end
 end
