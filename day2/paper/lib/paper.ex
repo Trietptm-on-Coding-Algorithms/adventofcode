af4e2967bc98bc807e179d78cbf565c19eded3f4
Andrew Kraut <akraut@pandora.com>
2016-03-22 08:50:02 -0700

    add initial helper methods

--- day2/paper/lib/paper.ex
+++ day2/paper/lib/paper.ex
@@ -1,2 +1,22 @@
 defmodule Paper do
+  def make_tuple(dimensions) do
+    dimensions
+    |> String.split("x")
+    |> strlist_to_int
+    |> List.to_tuple
+  end
+
+  defp strlist_to_int(strlist) do
+    for item <- strlist do
+      String.to_integer(item)
+    end
+  end
+
+  def make_sides({l, w, h}) do
+    [
+      2*l*w,
+      2*w*h,
+      2*h*l
+    ]
+  end
 end
