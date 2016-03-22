86efdfec60d11a2243e64aff6b6f51ea4292096a
Andrew Kraut <akraut@pandora.com>
2016-03-22 14:20:14 -0700

    Add tests for part 2

--- day2/paper/test/paper_test.exs
+++ day2/paper/test/paper_test.exs
@@ -29,4 +29,28 @@ defmodule PaperTest do
   test "1x1x10 needs 42 sqft" do
     assert 42 == Paper.surface?({1,1,10})
   end
+
+  test "2x3x4 needs 10 ft ribbon" do
+    assert 10 == Paper.ribbon?({2,3,4})
+  end
+
+  test "2x3x4 needs 24 ft bow" do
+    assert 24 == Paper.bow?({2,3,4})
+  end
+
+  test "2x3x4 totals 34 ft ribbon" do
+    assert 34 == Paper.total_ribbon?({2,3,4})
+  end
+
+  test "1x1x10 needs 4 ft ribbon" do
+    assert 4 == Paper.ribbon?({1,1,10})
+  end
+
+  test "1x1x10 needs 10ft bow" do
+    assert 10 == Paper.bow?({1,1,10})
+  end
+
+  test "1x1x10 totals 14 ft ribbon" do
+    assert 14 == Paper.total_ribbon?({1,1,10})
+  end
 end
