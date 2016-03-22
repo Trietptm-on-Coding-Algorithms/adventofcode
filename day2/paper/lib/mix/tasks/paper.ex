1e3c1913d6fa712d32fd1a1281a5a3e2874b66b4
Andrew Kraut <akraut@pandora.com>
2016-03-22 11:51:39 -0700

    Create Mix task to process a manifest file

---
+++ day2/paper/lib/mix/tasks/paper.ex
@@ -0,0 +1,19 @@
+defmodule Mix.Tasks.Paper.Manifest do
+  use Mix.Task
+
+  @shortdoc "Process a manifest of packages"
+
+  def run(args) do
+    File.stream!(args)
+    |> Enum.map(&clean_entry(&1))
+    |> Enum.map(&Paper.total?(&1))
+    |> Enum.sum
+    |> IO.inspect
+  end
+
+  defp clean_entry (entry) do
+    entry
+    |> to_string
+    |> String.strip
+  end
+end
