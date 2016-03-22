3f2214853660390ca774035dd502fa6d5743c93d
Andrew Kraut <akraut@gmail.com>
2016-03-21 23:57:01 -0700

    Setup tests from problem

--- day2/paper/test/paper_test.exs
+++ day2/paper/test/paper_test.exs
@@ -5,4 +5,28 @@ defmodule PaperTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "2x3x4 totals 58" do
+    assert 58 == Paper.total?({2,3,4})
+  end
+
+  test "2x3x4 needs 6 slack" do
+    assert 6 == Paper.slack?({2,3,4})
+  end
+
+  test "2x3x4 needs 52 sqft" do
+    assert 52 == Paper.base?({2,3,4})
+  end
+
+  test "1x1x10 totals 43" do
+    assert 43 == Paper.total?({1,1,10})
+  end
+
+  test "1x1x10 needs 1 slack" do
+    assert 1 == Paper.slack?({1,1,10})
+  end
+
+  test "1x1x10 needs 42 sqft" do
+    assert 42 == Paper.base?({1,1,10})
+  end
 end
